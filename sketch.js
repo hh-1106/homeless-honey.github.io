e32f5095b31edd1e135c66cddc051440c5d3dc92
homeless-honey <46677079+homeless-honey@users.noreply.github.com>
2019-08-19 17:58:52 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,7 @@
+var x = 200
+var y = 200
+
+
 function setup() {
   createCanvas(400, 400)
   rectMode(CENTER)
@@ -8,5 +12,9 @@ function setup() {
 function draw() {
   background(200, 0, 200)
   fill(0,255,255)
-  rect(200, 200, 100, 100, 10)
+  rect(x, y, 100, 100, 10)
+
+  x += random(-5,5)
+  y += random(-5,5)
+
 }
