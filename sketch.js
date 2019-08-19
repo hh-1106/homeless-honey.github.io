30cb6839d6220502d0a272f776b253d4e2a797a7
homeless-honey <46677079+homeless-honey@users.noreply.github.com>
2019-08-19 17:56:40 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,11 @@
-function setup(){
-  createCanvas(400,400)
-  background(200,0,200)
+function setup() {
+  createCanvas(400, 400)
+  rectMode(CENTER)
 
   createP('xswl')
+}
+
+function draw() {
+  background(200, 0, 200)
+  rect(200, 200, 100, 100, 10)
 }
