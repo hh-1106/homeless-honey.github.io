2114f9858319e07fe522fa499bf363ae1a5b9649
homeless-honey <46677079+homeless-honey@users.noreply.github.com>
2019-08-20 16:04:50 +0800

    chrismas

--- sketch.js
+++ sketch.js
@@ -1,24 +1,64 @@
-var x = 200
-var y = 200
+var song
+let snowflakes = [] // array to hold snowflake objects
 
+
+function preload() {
+  song = loadSound('assets/Chrismas.mp3')
+}
 
 function setup() {
-  createCanvas(400, 400)
-  rectMode(CENTER)
+  createCanvas(414, 736)
+  fill(240)
+  noStroke()
 
-  // createP('xswl')
-  createP('11.13')
+  song.loop()
 }
 
 function draw() {
-  background(200, 0, 200)
-  fill(0,255,255)
-  rect(x, y, 60, 60, 10)
+  background('brown')
+  let t = frameCount / 60 // update time
 
+  // create a random number of snowflakes each frame
+  for (let i = 0; i < random(5); i++) {
+    snowflakes.push(new snowflake()) // append snowflake object
+  }
 
-  fill(0,155,255)
-  rect(x/2, y/2, 60, 60, 10)
-  x += random(-5,5)
-  y += random(-5,5)
+  // loop through snowflakes with a for..of loop
+  for (let flake of snowflakes) {
+    flake.update(t) // update snowflake position
+    flake.display() // draw snowflake
+  }
+}
 
+// snowflake class
+function snowflake() {
+  // initialize coordinates
+  this.posX = 0
+  this.posY = random(-50, 0)
+  this.initialangle = random(0, 2 * PI)
+  this.size = random(2, 5)
+
+  // radius of snowflake spiral
+  // chosen so the snowflakes are uniformly spread out in area
+  this.radius = sqrt(random(pow(width / 2, 2)))
+
+  this.update = function(time) {
+    // x position follows a circle
+    let w = 0.6 // angular speed
+    let angle = w * time + this.initialangle
+    this.posX = width / 2 + this.radius * sin(angle)
+
+    // different size snowflakes fall at slightly different y speeds
+    this.posY += pow(this.size, 0.5)
+
+    // delete snowflake if past end of screen
+    if (this.posY > height) {
+      let index = snowflakes.indexOf(this)
+      snowflakes.splice(index, 1)
+    }
+  }
+
+  this.display = function() {
+    ellipse(this.posX, this.posY, this.size)
+  }
 }
